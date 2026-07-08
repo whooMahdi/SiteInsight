8c7753c421725e883b3adb9e40315707d98c7c63
mhrohani1385 <M.H.Rohani1385@gmail.com>
2026-07-08 08:27:44 +0330

    PageFactory

--- source/page.py
+++ source/page.py
@@ -8,11 +8,6 @@ class PageContent:
     class ContentSnippet(ABC):
         @property
         @abstractmethod
-        def text_on_image_weight(self) -> tuple[float, float]:
-            pass
-        
-        @property
-        @abstractmethod
         def raw_text(self) -> str:
             pass
 
@@ -20,9 +15,6 @@ class PageContent:
         def __init__(self, image_url: str, description : Optional[str] = None):
             self.image_url: str = image_url
             self.description: Optional[str] = description
-        @property
-        def text_on_image_weight(self) -> tuple[float, float]:
-            return (0 , 1)
         @property
         def raw_text(self) -> str:
             return self.image_url + (self.description or "")
@@ -32,19 +24,12 @@ class PageContent:
             self.video_url: str = video_url
             self.description: Optional[str] = description
         @property
-        def text_on_image_weight(self) -> tuple[float, float]:
-            return (0.2 , 1)
-        @property
         def raw_text(self) -> str:
             return self.video_url + (self.description or "")
 
     class TextSnippet(ContentSnippet):
         def __init__(self, text: str):
             self.text: str = text
-        @property
-        def text_on_image_weight(self) -> tuple[float, float]:
-            r = len(self.text) // CHARACTER_PER_IMAGE_RATIO
-            return (r, r)
         @property
         def raw_text(self) -> str:
             return self.text
@@ -53,15 +38,12 @@ class PageContent:
         def __init__(self, link: str):
             self.link: str = link
         @property
-        def text_on_image_weight(self) -> tuple[float, float]:
-            return (0.25, 1)
-        @property
         def raw_text(self) -> str:
             return self.link
 
     def __init__(self, snippet_list: Optional[list[ContentSnippet]] = None):
         self._snippet_list: list[PageContent.ContentSnippet] = snippet_list or list()
-        self._link_snippet_list: list[PageContent.LinkSnippet] = 
+        self._link_snippet_list: list[PageContent.LinkSnippet] = \
             list() if snippet_list is None else list(filter(lambda x: isinstance(x, PageContent.LinkSnippet), snippet_list)) # type: ignore
 
     def add_snippet(self, snippet: ContentSnippet):
@@ -82,6 +64,10 @@ class PageContent:
             if isinstance(s, PageContent.ImageSnippet | PageContent.VideoSnippet):
                 yield s
 
+    def get_text_snippets(self):
+        for s in self.get_page_snippets():
+            if isinstance(s, PageContent.TextSnippet):
+                yield s
 class WebPage:
     def __init__(self, url: URL, page_title: str, content: PageContent):
         self.url: URL = url
@@ -94,21 +80,33 @@ class WebPage:
 
 class ArticlePage(WebPage):
 
-    def __init__(self, url: URL, page_title: str, content: PageContent, author: Optional[str] = None):
+    def __init__(self, url: URL, page_title: str, content: PageContent, author_snippet: Optional[PageContent.ContentSnippet] = None):
         super().__init__(url, page_title, content)
-        self.author = author
-        self.catigory_name = "[article]"            
+
+        if author_snippet is None:
+            for s in content.get_page_snippets():
+                if "author" in s.raw_text:
+                    author_snippet = s
+        self.author_snippet = author_snippet
 
 class ProductPage(WebPage):
 
-    def __init__(self, url: URL, page_title: str, content: PageContent, product_price: Optional[int] = None):
+    def __init__(self, url: URL, page_title: str, content: PageContent, price_snippet: Optional[PageContent.TextSnippet] = None):
         super().__init__(url, page_title, content)
-        self.product_price = product_price
+        if price_snippet is None:
+            for t in content.get_text_snippets():
+                if "price" in t.text.lower():
+                    price_snippet = t
+        self.price_snippet = price_snippet
 
 class GalleryPage(WebPage):
 
     def __init__(self, url: URL, page_title: str, content: PageContent):
         super().__init__(url, page_title, content)
+
+    @property
+    def media_snippets(self):
+        return self.content.get_media_snippets()
 
 class PageFactory():
     def __init__(self, url: URL, page_title: str, content: PageContent):
@@ -128,29 +126,24 @@ class PageFactory():
             text = snippet.raw_text.lower()
             for k in keywords:
                 points += text.count(k.lower())
+        titile_l = self.page_title.lower()
+        for k in keywords:
+            points += titile_l.count(k.lower())
         return points
-
-    @property
-    def total_text_on_image_weight(self) -> float:
-        num = 0.0
-        denom = 0.0
-
-        for s in self.content.get_page_snippets():
-            w = s.text_on_image_weight
-            num += w[0]
-            denom += w[1]
-
-        return num / denom
     
     @property
     def media_count(self):
         return len(list(self.content.get_media_snippets()))
     
     def create(self):
-        gallery_point = ((0.4 if self.media_count > 10 else 0.0) + self.total_text_on_image_weight) * 
-        if :
+        text_product_similarity = self._similarity_points_in_text(["price", "product", "stock", "store"])
+        url_product_similarity = self._similarity_points_in_url(["/p/", "product", "item"])
+        if text_product_similarity >= 2 or (text_product_similarity > 0 and url_product_similarity > 0):
+            return ProductPage(self.url, self.page_title, self.content)
+        elif self.media_count > 5 or self._similarity_points_in_url(["gallery"]) > 0 or self._similarity_points_in_text(["gallery"]) > 0:
             return GalleryPage(self.url, self.page_title, self.content)
-        elif self._similarity_points_in_text(["price", "product", "stock", "store"]) + self._similarity_points_in_url(["/p/", "product", "item"]) * 2 > 2:
+        else:
+            return ArticlePage(self.url, self.page_title, self.content)
     
 
     @staticmethod
--- test/page-test.py
+++ test/page-test.py
@@ -1,4 +1,23 @@
-from source.page import PageContent, WebPage
+from source.page import PageContent, WebPage, PageFactory, GalleryPage, ProductPage, ArticlePage
+from source.url_utils import URL
 
 content = PageContent()
-content.add_snippet(PageContent.TextSnippet("hello"))
+content.add_snippet(PageContent.TextSnippet("Page 1"))
+content.add_snippet(PageContent.TextSnippet("product 2"))
+content.add_snippet(PageContent.ImageSnippet("https://example.com/image-2.jpg"))
+content.add_snippet(PageContent.LinkSnippet("https://example.com/link-1"))
+content.add_snippet(PageContent.VideoSnippet("https://example.com/video-1", description="Video 1"))
+content.add_snippet(PageContent.TextSnippet("Price : 100 $"))
+
+page = PageFactory.create_page(URL("example.com"), "title", content);
+
+if isinstance(page, ProductPage):
+    s = page.price_snippet
+    if s is None:
+        s = ""
+    else:
+        s = s.raw_text
+    print(s)
+
+# poetry run python -m test.page-test
+# prints :: Price : 100 $
