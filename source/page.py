95fd4dbb36133ca29136977ec415249fa5c2fdcc
mhrohani1385 <M.H.Rohani1385@gmail.com>
2026-07-08 06:58:36 +0330

    before-expremental

--- source/page.py
+++ source/page.py
@@ -1,5 +1,6 @@
 from abc import ABC, abstractmethod
 from typing import Optional
+from source.network import URL
 
 CHARACTER_PER_IMAGE_RATIO = 380
 
@@ -9,6 +10,11 @@ class PageContent:
         @abstractmethod
         def text_on_image_weight(self) -> tuple[float, float]:
             pass
+        
+        @property
+        @abstractmethod
+        def raw_text(self) -> str:
+            pass
 
     class ImageSnippet(ContentSnippet):
         def __init__(self, image_url: str, description : Optional[str] = None):
@@ -17,6 +23,20 @@ class PageContent:
         @property
         def text_on_image_weight(self) -> tuple[float, float]:
             return (0 , 1)
+        @property
+        def raw_text(self) -> str:
+            return self.image_url + (self.description or "")
+
+    class VideoSnippet(ContentSnippet):
+        def __init__(self, video_url: str, description : Optional[str] = None):
+            self.video_url: str = video_url
+            self.description: Optional[str] = description
+        @property
+        def text_on_image_weight(self) -> tuple[float, float]:
+            return (0.2 , 1)
+        @property
+        def raw_text(self) -> str:
+            return self.video_url + (self.description or "")
 
     class TextSnippet(ContentSnippet):
         def __init__(self, text: str):
@@ -25,6 +45,9 @@ class PageContent:
         def text_on_image_weight(self) -> tuple[float, float]:
             r = len(self.text) // CHARACTER_PER_IMAGE_RATIO
             return (r, r)
+        @property
+        def raw_text(self) -> str:
+            return self.text
 
     class LinkSnippet(ContentSnippet):
         def __init__(self, link: str):
@@ -32,53 +55,105 @@ class PageContent:
         @property
         def text_on_image_weight(self) -> tuple[float, float]:
             return (0.25, 1)
+        @property
+        def raw_text(self) -> str:
+            return self.link
 
     def __init__(self, snippet_list: Optional[list[ContentSnippet]] = None):
         self._snippet_list: list[PageContent.ContentSnippet] = snippet_list or list()
+        self._link_snippet_list: list[PageContent.LinkSnippet] = 
+            list() if snippet_list is None else list(filter(lambda x: isinstance(x, PageContent.LinkSnippet), snippet_list)) # type: ignore
 
     def add_snippet(self, snippet: ContentSnippet):
+        if isinstance(snippet, PageContent.LinkSnippet):
+            self._link_snippet_list.append(snippet)
         self._snippet_list.append(snippet)
+    
+    def get_page_snippets(self):
+        for s in self._snippet_list:
+            yield s
+    
+    @property
+    def page_links_list(self):
+        return self._link_snippet_list
+    
+    def get_media_snippets(self):
+        for s in self.get_page_snippets():
+            if isinstance(s, PageContent.ImageSnippet | PageContent.VideoSnippet):
+                yield s
+
+class WebPage:
+    def __init__(self, url: URL, page_title: str, content: PageContent):
+        self.url: URL = url
+        self.page_title: str = page_title
+        self.content: PageContent = content
+        self.refered_in: list['WebPage'] = list()     
+
+    def add_refered_in(self, page: 'WebPage'):
+        self.refered_in.append(page)
+
+class ArticlePage(WebPage):
+
+    def __init__(self, url: URL, page_title: str, content: PageContent, author: Optional[str] = None):
+        super().__init__(url, page_title, content)
+        self.author = author
+        self.catigory_name = "[article]"            
+
+class ProductPage(WebPage):
+
+    def __init__(self, url: URL, page_title: str, content: PageContent, product_price: Optional[int] = None):
+        super().__init__(url, page_title, content)
+        self.product_price = product_price
+
+class GalleryPage(WebPage):
+
+    def __init__(self, url: URL, page_title: str, content: PageContent):
+        super().__init__(url, page_title, content)
+
+class PageFactory():
+    def __init__(self, url: URL, page_title: str, content: PageContent):
+        self.url = url
+        self.page_title = page_title
+        self.content = content
+
+    def _similarity_points_in_url(self, keywords: list[str]) -> int:
+        points = 0
+        for k in keywords:
+            points += self.url.path.lower().count(k)
+        return points
+
+    def _similarity_points_in_text(self, keywords : list[str]) -> int:
+        points = 0
+        for snippet in self.content.get_page_snippets():
+            text = snippet.raw_text.lower()
+            for k in keywords:
+                points += text.count(k.lower())
+        return points
 
     @property
     def total_text_on_image_weight(self) -> float:
         num = 0.0
         denom = 0.0
 
-        for s in self._snippet_list:
+        for s in self.content.get_page_snippets():
             w = s.text_on_image_weight
             num += w[0]
             denom += w[1]
 
         return num / denom
+    
+    @property
+    def media_count(self):
+        return len(list(self.content.get_media_snippets()))
+    
+    def create(self):
+        gallery_point = ((0.4 if self.media_count > 10 else 0.0) + self.total_text_on_image_weight) * 
+        if :
+            return GalleryPage(self.url, self.page_title, self.content)
+        elif self._similarity_points_in_text(["price", "product", "stock", "store"]) + self._similarity_points_in_url(["/p/", "product", "item"]) * 2 > 2:
+    
 
-class WebPage:
-    def __init__(self, url: str, page_title: str, content: PageContent, raw_html: str, links: list['WebPage'], refered_in: list['WebPage']) -> None:
-        self.url: str = url
-        self.page_title: str = page_title
-        self.content: PageContent = content
-        self.raw_html: str = raw_html
-        self.links: list[WebPage]
-
-# class ArticlePage(WebPage):
-# def __init__(self, url: str, html_content: str, depth: int, author: Optional[str] =
-# None) -> None:
-# super().__init__(url, html_content, depth)
-# self.author: Optional[str] = author
-# self.category = "article"
-# class ProductPage(WebPage):
-# def __init__(self, url: str, html_content: str, depth: int, price: Optional[str] =
-# None) -> None:
-# super().__init__(url, html_content, depth)
-# self.price: Optional[str] = price
-# self.category = "product"
-# class GalleryPage(WebPage):
-# def __init__(self, url: str, html_content: str, depth: int, image_urls:
-# Optional[List[str]] = None) -> None:
-# super().__init__(url, html_content, depth)
-# self.image_urls: List[str] = image_urls or []
-# self.category = "gallery"
-# class PageFactory:
-# @staticmethod
-# def create_page(url: str, html_content: str, depth: int, image_count: int,
-# contains_price: bool) -> WebPage:
-# pass
+    @staticmethod
+    def create_page(url: URL, page_title: str, content: PageContent) -> WebPage:
+        return PageFactory(url, page_title, content).create()
+    
