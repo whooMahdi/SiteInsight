119243c83c17d0bd6e0586c2eb5040a89e0cfdf5
whooMahdi <whoo.mahdi@gmail.com>
2026-07-17 22:41:09 +0330

    add shortner util to utils.py

--- source/utils.py
+++ source/utils.py
@@ -1,7 +1,26 @@
 import os
+from typing import Optional
+
 
 def get_abs_path(path: str):
     try:
         return os.path.abspath(path)
     except:
         return "(No file exists to find abs path)"
+    
+
+def shortner(
+        value,
+        head: int = 25,
+        tail: int = 15,
+        threshold: Optional[int] = None
+    ) -> str:
+    threshold = threshold or (head + tail)
+    text = str(value) if not isinstance(value, str) else value
+    text = " ".join(text.split())
+
+    return (
+        f"{text[:head]}...{text[-tail:] if tail else ''}"
+        if len(text) > threshold else
+        text
+    )
